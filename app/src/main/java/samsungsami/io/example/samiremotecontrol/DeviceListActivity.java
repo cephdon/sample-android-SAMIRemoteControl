07add7e9e4198e9d54521c307cb278764b99b3e4
Yujing Wu <yujingwu@gmail.com>
2015-03-30 10:35:18 -0700

    OM-216 apply Wilfred's fix which ensure one AsyncTask make only one call to SAMI

--- app/src/main/java/samsungsami/io/example/samiremotecontrol/DeviceListActivity.java
+++ app/src/main/java/samsungsami/io/example/samiremotecontrol/DeviceListActivity.java
@@ -19,29 +19,35 @@ package samsungsami.io.example.samiremotecontrol;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 
+import io.samsungsami.api.DeviceTypesApi;
 import io.samsungsami.api.UsersApi;
 import io.samsungsami.client.ApiException;
 import io.samsungsami.model.Device;
 import io.samsungsami.model.DeviceType;
-import io.samsungsami.model.DeviceTypesEnvelope;
+import io.samsungsami.model.DeviceTypeEnvelope;
 import io.samsungsami.model.DevicesEnvelope;
 
 public class DeviceListActivity extends Activity {
 
-    private ListView mainListView;
+    private DeviceTypesApi deviceTypesApi;
+
     private ArrayList<String> deviceNames = new ArrayList<String>();
     private ArrayList<Device> devices = new ArrayList<Device>();
+    private HashSet<String> devicesTypeIds = new HashSet<String>();
     private ArrayAdapter<String> listAdapter;
+
+    private ListView mainListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,52 +55,54 @@ public class DeviceListActivity extends Activity {
         setContentView(R.layout.activity_device_list);
         mainListView = (ListView) findViewById( R.id.mainListView );
         listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, deviceNames);
+
         // Setup Users API
-        UsersApi api = new UsersApi();
-        api.setBasePath(SamiHelper.SAMIHUB_BASE_PATH);
-        api.getInvoker().addDefaultHeader("Authorization", "bearer " + SamiHelper.getAccessToken());
-        new CallUsersApiInBackground().execute(api);
+        UsersApi usersApi = new UsersApi();
+        usersApi.setBasePath(SamiHelper.SAMIHUB_BASE_PATH);
+        usersApi.getInvoker().addDefaultHeader("Authorization", "bearer " + SamiHelper.getAccessToken());
 
+        // Setup DeviceTypes API
+        deviceTypesApi = new DeviceTypesApi();
+        deviceTypesApi.setBasePath(SamiHelper.SAMIHUB_BASE_PATH);
+        deviceTypesApi.getInvoker().addDefaultHeader("Authorization", "bearer " + SamiHelper.getAccessToken());
 
+        // Call Users API
+        new CallUsersApiInBackground().execute(usersApi);
     }
     /**
      * AsyncTask to retrieve the user devices and devices types from the data collected
      * in the User API request
      */
-    class CallUsersApiInBackground extends android.os.AsyncTask<UsersApi, Void, Object[]> {
+    class CallUsersApiInBackground extends android.os.AsyncTask<UsersApi, Void, DevicesEnvelope> {
 
         /**
          * Query the Users API
          *
          * @param apis : Users API
-         * @return Object[] (Object[0] -> DevicesEnvelope and Object[1] -> DeviceTypesEnvelope)
          */
         @Override
-        protected Object[] doInBackground(UsersApi... apis) {
-            Object[] retVal = new Object[2];
+        protected DevicesEnvelope doInBackground(UsersApi... apis) {
+            DevicesEnvelope result = null;
             try {
-                retVal[0] = apis[0].getUserDevices(0, 100, true, SamiHelper.getUserId());
-                retVal[1] = apis[0].getUserDeviceTypes(0, 100, true, SamiHelper.getUserId());
-
-                // For the moment, all devices types for which the user has an access are gathered.
-                // In the future, according to the number of the user, it would be more
-                // appropriate to make separate request per Device Type Id to find their
-                // names.
+                result = apis[0].getUserDevices(0, 100, true, SamiHelper.getUserId());
 
             } catch (ApiException e) {
-              Log.e("DeviceList", e.toString());
+              Log.d("DeviceList", e.toString());
             }
-            return retVal;
+            return result;
         }
 
         /**
          * Process the data collected from the API
          *
-         * @param results (Object[0] -> DevicesEnvelope and Object[1] -> DeviceTypesEnvelope)
+         * @param devicesEnvelope
          */
         @Override
-        protected void onPostExecute(Object[] results) {
-            DevicesEnvelope devicesEnvelope = (DevicesEnvelope) results[0];
+        protected void onPostExecute(DevicesEnvelope devicesEnvelope) {
+
+            if (devicesEnvelope == null)
+                // No permission to any DeviceType.
+                return;
 
             if (devicesEnvelope.getData().getDevices().size() == 0)
                 return;
@@ -105,19 +113,42 @@ public class DeviceListActivity extends Activity {
                 devices.add(device);
             }
 
-            HashSet<String> devicesTypeIds = new HashSet<String>();
             // Collect all devices types ID
             for (Device device : devicesEnvelope.getData().getDevices())
                 devicesTypeIds.add(device.getDtid());
 
-            // Find the corresponding device type names
-            DeviceTypesEnvelope deviceTypesEnvelope = (DeviceTypesEnvelope) results[1];
-            final HashMap<String, String> deviceTypeIdToName = new HashMap<String, String>();
-            for (DeviceType deviceType : deviceTypesEnvelope.getData().getDeviceTypes())
-                if (devicesTypeIds.contains(deviceType.getId())) {
-                    deviceTypeIdToName.put(deviceType.getId(), deviceType.getName());
-                }
+            new CallDeviceTypesApiInBackground().execute(deviceTypesApi);
 
+        }
+    }
+
+    class CallDeviceTypesApiInBackground extends android.os.AsyncTask<DeviceTypesApi, Void, List<DeviceTypeEnvelope>> {
+
+        @Override
+        protected List<DeviceTypeEnvelope> doInBackground(DeviceTypesApi... apis) {
+            List<DeviceTypeEnvelope> results = new ArrayList<DeviceTypeEnvelope>();
+            int i = 0;
+            for (String dtid : devicesTypeIds) {
+                try
+                {
+                    results.add(apis[0].getDeviceType(dtid));
+                }catch (ApiException e)
+                {
+                    results.add(null);
+                    e.printStackTrace();
+                }
+            }
+            return results;
+        }
+
+        @Override
+        protected void onPostExecute(List<DeviceTypeEnvelope> results) {
+            // Find the corresponding device type names
+            final HashMap<String, String> deviceTypeIdToName = new HashMap<String, String>();
+            for (int i = 0; i < results.size(); i++) {
+                DeviceType deviceType = results.get(i).getData();
+                deviceTypeIdToName.put(deviceType.getId(), deviceType.getName());
+            }
             mainListView.setAdapter(listAdapter);
             mainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
